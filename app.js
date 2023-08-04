8b79193ef543f7a4cf556a92bf44ed24628bc854
Jiyu Jang <apryll7995@gmail.com>
2023-08-04 19:58:04 +0900

    [UPDATE] sequelize 초기 설정

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const { sequelize } = require("./models");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
+sequelize
+  .sync()
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch(err => {
+    console.error("데이터베이스 연결 error\r\n", err);
+  });
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
